68dc60d88eef931af581f4867582dfb8d34d4da8
Bradley Wyatt <bwyatt@psmpartners.com>
2024-01-10 21:52:42 -0600

    improved performance, fixed secretdisplayname

--- Azure/AppSecretExpiration_SendtoTeams.ps1
+++ Azure/AppSecretExpiration_SendtoTeams.ps1
@@ -61,47 +61,44 @@ Function Get-MSGraphRequest {
 
 $tokenResponse = Connect-MSGraphAPI -AppID $AppID -TenantID $TenantID -AppSecret $AppSecret
 $array = @()
-Get-MSGraphRequest -AccessToken $tokenResponse.access_token -Uri "https://graph.microsoft.com/v1.0/applications/" |  Foreach-Object {
-    [string]$secretdisplayName = $_.passwordCredentials.displayName
-    [string]$id = $_.id
-    [string]$displayname = $_.displayName
-
-    #If there are more than one password credentials, we need to get the expiration of each one
-    if ($_.passwordCredentials.endDateTime.count -gt 1) {
-        $endDates = $_.passwordCredentials.endDateTime
-        [int[]]$daysUntilExpiration = @()
-        foreach ($Date in $endDates) {
-            $Date = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($Date, 'Central Standard Time')
-            $daysUntilExpiration += (New-TimeSpan -Start ([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::Now, "Central Standard Time")) -End $Date).Days
+$apps = Get-MSGraphRequest -AccessToken $tokenResponse.access_token -Uri "https://graph.microsoft.com/v1.0/applications/" 
+foreach ($app in $apps) {
+    $app.passwordCredentials | foreach-object {
+        #If there is a secret with a enddatetime, we need to get the expiration of each one
+        if ($_.endDateTime -ne $null) {
+            [system.string]$secretdisplayName = $_.displayName
+            [system.string]$id = $app.id
+            [system.string]$displayname = $app.displayName
+            $Date = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($_.endDateTime, 'Central Standard Time')
+            [int32]$daysUntilExpiration = (New-TimeSpan -Start ([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::Now, "Central Standard Time")) -End $Date).Days
+            
+            if (($daysUntilExpiration -ne $null) -and ($daysUntilExpiration -le $expirationDays)) {
+                $array += $_ | Select-Object @{
+                    name = "id"; 
+                    expr = { $id } 
+                }, 
+                @{
+                    name = "displayName"; 
+                    expr = { $displayName } 
+                }, 
+                @{
+                    name = "secretName"; 
+                    expr = { $secretdisplayName } 
+                },
+                @{
+                    name = "daysUntil"; 
+                    expr = { $daysUntilExpiration } 
+                }
+            }
+            $daysUntilExpiration = $null
+            $secretdisplayName = $null
         }
     }
-    ElseIf ($_.passwordCredentials.endDateTime.count -eq 1) {
-        $Date = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($_.passwordCredentials.endDateTime, 'Central Standard Time')
-        $daysUntilExpiration = (New-TimeSpan -Start ([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::Now, "Central Standard Time")) -End $Date).Days 
-    }
-
-    $daysUntilExpiration | foreach-object { 
-        if (($_ -ne $null) -and ($_ -le $expirationDays)) {
-            $array += $_ | Select-Object @{
-                name = "id"; 
-                expr = { $id } }, 
-                @{
-                name = "displayName"; 
-                expr = { $displayName } }, 
-                @{
-                name = "secretdisplayName"; 
-                expr = { $secretdisplayName } },
-                @{
-                name = "daysUntil"; 
-                expr = { $_ } }
-        }
-    }
-    $daysUntilExpiration = $null
 }
 
 if ($array.count -ne 0) {
     Write-output "Sending Teams Message"
-    $textTable = $array | Sort-Object daysUntil | select-object displayName, secretdisplayname, daysUntil | ConvertTo-Html
+    $textTable = $array | Sort-Object daysUntil | select-object displayName, secretName, daysUntil | ConvertTo-Html
     $JSONBody = [PSCustomObject][Ordered]@{
         "@type"      = "MessageCard"
         "@context"   = "<http://schema.org/extensions>"
